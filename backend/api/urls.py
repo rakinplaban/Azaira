079c5005802b0a674f0d7fe2440e25596b4062c1
rakinplaban <rakinshahriar54@gmail.com>
2023-07-23 11:06:04 +0600

    token authentication done.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,6 +5,5 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('', views.index, name='api_index'),
     path('register',views.registration_view,name='api_register'),
-    path('login',views.api_login_view,name='api_login'),
-    path('logout',views.api_logout_view,name='api_logout'),
+    path('login',obtain_auth_token,name='api_login'),
 ]
